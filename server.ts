a545470cd175d73f45669ac7cb1c2d3bb0bf75c5
Camila <camilaroson@hotmail.com>
2021-03-17 22:48:29 -0300

    Arreglos SQLite3 y MYSQL

--- server.ts
+++ server.ts
@@ -1,18 +1,3 @@
-//KNEX Y SQLITE//
-const {sqlite3} = require('./DB/SQLite.db')
-const knex = require('knex')(sqlite3)
-
-//CREO LA TABLA
-/*
-knex.schema.createTable('mensajes', (table: { string: (arg0: string, arg1: number) => void }) =>{
-  table.string('inputMensaje',50)
-  table.string('email',100)
-})
-.then(()=> console.log('Se creo la tabla'))
-.catch((err:any) => console.log(err))
-.finally(()=> knex.destroy())
-*/
-
 //inicializacion express y socket
 
 import { Socket } from 'dgram';
@@ -21,34 +6,47 @@ const app = express()
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 import moment from 'moment';
+import fs from "fs"
 
 var router = express.Router()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
-
 app.use(express.static('public'))
 
 
+//SQLITE//
+const {sqlite3} = require('./DB/SQLite.db')
+const knex = require('knex')(sqlite3)
 
-//cargo modulo handlebars 
-import handlebars from 'express-handlebars'
+//MYSQL//
+/*
+const {mysql} = require('./DB/MYSQL.db')
+const knex = require('knex')(mysql)
 
-//configuracion handle
+*/
 
-app.engine (
-    "hbs",
-    handlebars({
-        extname: ".hbs",
-        defaultLayout: 'index.hbs',
-        layoutsDir: __dirname + "/views/layouts",
-        partialsDir: __dirname + '/views/partials/'
-    })
+//CREO LA TABLA DE MENSAJES - SQLITE
+/*
+knex.schema.createTable('mensajes', (table: { string: (arg0: string, arg1: number) => void }) =>{
+  table.string('inputMensaje',50)
+  table.string('email',100)
+})
+.then(()=> console.log('Se creo la tabla'))
+.catch((err:any) => console.log(err))
+*/
 
-)
+//CREO LA TABLA DE PRODUCTOS - MYSQL
+/*
+knex.schema.createTable('productos', (table: { increments: (arg0: string) => void; string: (arg0: string, arg1: number) => void; integer: (arg0: string, arg1: number) => void }) =>{
+  table.increments('id')
+  table.string('titulo',50)
+  table.integer('precio',100)
+  table.string('imagen',50)
+})
+.then(()=> console.log('Se creo la tabla'))
+.catch((err:any) => console.log(err))
 
-app.set("view engine","hbs")
-app.set("views","./views")
-
+*/
 
 
 // routers
@@ -69,26 +67,17 @@ io.on('connection', (socket:any) => {
        // console.log(data)
         io.emit('mensaje del chat',data)
         //GUARDAR SQLITE
-        knex('mensajes').insert(data)
+      knex('mensajes').insert(data)
         .then(()=>console.log("mensaje guardado"))
         .catch((err:any)=>console.log(err))
-        .finally(()=>knex.destroy())
-        //VER PORQUE GUARDA UNA SOLA VEZ Y DESPUES ROMPE. 
+
+        //CREO ARCHIVO DE TEXTO
+        const guardarMensajes = JSON.stringify(data)
+        fs.appendFileSync('./productos.txt', '\n' + guardarMensajes)
+    })
+      
     })
 
-})
-
-
-
-
-// Listen - cambia a http
-
-/*
-app.listen(8080 , () =>{
-    console.log('Servidor listo!')
-})
-
-*/
 
 http.listen(3333, () => {
     console.log('Servidor listo :)')
